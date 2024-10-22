b8b71473ef6791769b003056cf694bc7c2a31970
Imran Chowdhury <imranchowdhury6093@gmail.com>
2024-10-22 13:39:58 +0600

    Update job_controller.dart
    Conversion of snapshots to list of appointments for the calendar view

--- lib/controllers/job_controller.dart
+++ lib/controllers/job_controller.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 
 //Fetching for mechanics
@@ -17,5 +18,25 @@ Stream<QuerySnapshot<Map<String, dynamic>>> getAdminBookings() {
   return FirebaseFirestore.instance
       .collectionGroup('jobs')
       .snapshots(); // Fetches all jobs across all mechanics
+}
+
+
+List<Appointment> getBookingsFromSnapshot(QuerySnapshot snapshot) {
+  return snapshot.docs.map((doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Appointment(
+      startTime: DateTime.parse(data['startDateTime']),
+      endTime: DateTime.parse(data['endDateTime']),
+      subject: data['bookingTitle'] ?? 'No Title',
+      notes: '${data['carMake']} ${data['carModel']}',
+    );
+  }).toList();
+}
+
+
+class BookingDataSource extends CalendarDataSource {
+  BookingDataSource(List<Appointment> source) {
+    appointments = source;
+  }
 }
 
